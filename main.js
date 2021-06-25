206c66afc95e30f7063b6ea7b3a75f79705e11f7
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2021-06-25 18:49:01 -0400

    Added Sweetalert Toasts

--- main.js
+++ main.js
@@ -2139,3 +2139,14 @@ if ( typeof document.addEventListener !== 'undefined' )
 		}
 	);
 } );
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-right',
+  iconColor: 'white',
+  customClass: {
+    popup: 'colored-toast'
+  },
+  showConfirmButton: false,
+  timer: 1500,
+  timerProgressBar: true
+})
